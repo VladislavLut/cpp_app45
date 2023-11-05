2c322312240d89f3d9f24ebdcbd0986613ce4ac7
VladislavLut <143324701+VladislavLut@users.noreply.github.com>
2023-11-05 11:51:42 +0200

    ex-5 was done

--- main.cpp
+++ main.cpp
@@ -1,63 +1,20 @@
 #include <iostream>
 using namespace std;
 
-void add(int* a, int* b, int* result) {
-    *result = *a + *b;
-}
-
-void subtract(int* a, int* b, int* result) {
-    *result = *a - *b;
-}
-
-void multiply(int* a, int* b, int* result) {
-    *result = *a * (*b);
-}
-
-void divide(int* a, int* b, float* result) {
-    if (*b != 0) {
-        *result = static_cast<float>(*a) / (*b);
-    }
-    else {
-        cout << "Error: Division by zero!" << endl;
-    }
-}
-
 int main() {
-    int num1, num2;
-    char op;
-    int resultInt;
-    float resultFloat;
+    const int arraySize = 5; 
+    int numbers[arraySize] = { 10, 20, 30, 40, 50 }; 
 
-    cout << "Enter the first number: ";
-    cin >> num1;
-    cout << "Enter operation (+, -, *, /): ";
-    cin >> op;
-    cout << "Enter the second number: ";
-    cin >> num2;
+    int sum = 0; 
 
-    switch (op) {
-    case '+':
-        add(&num1, &num2, &resultInt);
-        cout << "Addition result: " << resultInt << endl;
-        break;
-    case '-':
-        subtract(&num1, &num2, &resultInt);
-        cout << "Subtraction result: " << resultInt << endl;
-        break;
-    case '*':
-        multiply(&num1, &num2, &resultInt);
-        cout << "Multiplication result: " << resultInt << endl;
-        break;
-    case '/':
-        divide(&num1, &num2, &resultFloat);
-        if (num2 != 0) {
-            cout << "Division result: " << resultFloat << endl;
-        }
-        break;
-    default:
-        cout << "Wrong operation!" << endl;
-        break;
+    int* ptr = numbers; 
+
+    for (int i = 0; i < arraySize; i++) {
+        sum += *ptr; 
+        ptr++; 
     }
+
+    cout << "The sum of array elements: " << sum << endl;
 
     return 0;
 }
